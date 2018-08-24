6e84e239a34d1bf50ca8cf3a4a381bf5bb8956d4
Anderson <andersonsantofarias@outlook.com>
2018-08-23 22:05:12 -0300

    adicionando método de calcularQtdGasto()

--- fumante.rb
+++ fumante.rb
@@ -5,4 +5,11 @@ class Fumante
 		@nome, @anos_fumando, @num_cigarro_dia, @preco_carteira = nome, anos_fumando, num_cigarro_dia, preco_carteira
 	end
 
+	def calcularQtdGasto()	
+		#@qtdGasto =  5 * 365
+		@qtdGasto =  (((@anos_fumando * 365) * @num_cigarro_dia) / 20) * @preco_carteira
+		puts "Nome: #{@nome}, Quantidade gasto em #{@anos_fumando} anos com cigarros de R$ #{@qtdGasto}"
+		puts "Alerta: O tabagismo é uma doença considerada pela OMS (Organização Mundial da Saúde) como a principal causa de morte evitável em todo mundo."	
+	end
+
 end
